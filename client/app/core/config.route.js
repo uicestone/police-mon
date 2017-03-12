0a16088029f584d5752ff815c238204b63e42577
Uice Lu <uicestone@gmail.com>
2017-03-12 21:38:57 +0800

    移除派出所项目有关代码

--- client/app/core/config.route.js
+++ client/app/core/config.route.js
@@ -7,7 +7,6 @@
                 var routes, setRoutes;
 
             routes = [
-                'suspect/list', 'suspect/wizard', 'station/map',
                 'ui/cards', 'ui/typography', 'ui/buttons', 'ui/icons', 'ui/grids', 'ui/widgets', 'ui/components', 'ui/timeline', 'ui/lists', 'ui/pricing-tables',
                 'map/maps',
                 'table/static', 'table/dynamic', 'table/responsive',
